3e9fa88956762cd13ed28fd3eaa0167abf883f8e
johyukrae <huu000008@gamil.com>
2025-02-28 20:06:02 +0900

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import styles from "./page.module.css";
 
 export default async function Home() {
-  return <div className={styles.page}>메인</div>;
+  return <div className={styles.page}>메a인</div>;
 }
